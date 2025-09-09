a4ea316f194355789ac144d1420183efc2ab8e63
Rodolfo Dagum <rodolfo.dagum@ukg.com>
2025-09-08 21:46:29 -0400

    PS-545846 Fix playwright setup to allow for debugging in VSCode

--- playwright-setup.cmd
+++ playwright-setup.cmd
@@ -1,6 +1,7 @@
 
 
 SET DOWNLOAD=FALSE
+SET LOCAL_BROWSERS=%PYTHON_VENV%\..\lib\site-packages\Browser\wrapper\node_modules\playwright-core\.local-browsers
 IF [%PLAYWRIGHT_BROWSERS_PATH%]==[] (
     SET DOWNLOAD=TRUE
     SET PLAYWRIGHT_BROWSERS_PATH=%USERPROFILE%\pw-browsers
@@ -17,13 +18,15 @@ IF NOT EXIST %DOWNLOAD_TIMESTAMP_FILE% (
 
 IF [%DOWNLOAD%] == [TRUE] (
     call subtitle.cmd Installing playwright browsers
-    call npm -g install playwright // Step apparently not needed. We'll need to evaluate when a chrome update is available
+    call npm -g install playwright
     call npx playwright install
+    call rfbrowser init --skip-browsers
     echo %DATE:~4,10% > %DOWNLOAD_TIMESTAMP_FILE%
+    REM The SymLink below is required to allow playwright to find the browsers when running tests with F5 in VSCode or
+    REM when running from the Testing section in VSCode
+    rmdir "%LOCAL_BROWSERS%" /q 2>nul
+    mklink /d "%LOCAL_BROWSERS%" "%PLAYWRIGHT_BROWSERS_PATH%"
     call subtitle-end.cmd Done installing playwright browsers
 )
 
-REM Steps apparently not needed. We'll need to evaluate when a chrome update is available
-REM SET LOCAL_BROWSERS=%PYTHON_VENV%\..\lib\site-packages\Browser\wrapper\node_modules\playwright\.local-browsers
-REM IF NOT EXIST %LOCAL_BROWSERS% mklink /d %LOCAL_BROWSERS% %PLAYWRIGHT_BROWSERS_PATH%
 exit /b 0
--- robot-setup.cmd
+++ robot-setup.cmd
@@ -10,16 +10,11 @@ call subtitle.cmd Setting up Robot Framework Requirements
 call %PYTHON_VENV%\activate.bat
 call subtitle.cmd Installing Browser library dependencies...
 call playwright-setup.cmd
-call rfbrowser init --skip-browsers
 call subtitle-end.cmd Done installing Browser library dependencies...
 
 
 IF ERRORLEVEL 1 EXIT /B 1
 call %PYTHON_VENV%\deactivate.bat
-
-@REM call choco source remove -n=chocolatey
-@REM call choco source add -n=ascentis-artifactory-prod -s=https://artifactory.ascentis.com/artifactory/api/nuget/ascentis-chocolatey
-@REM call choco install vcredist140
 
 popd
 endlocal
